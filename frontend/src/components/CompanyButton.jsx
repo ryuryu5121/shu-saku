fc49ffc1aba8e88617daa817b81629e3710cc1bf
muto2028 <166196097+muto2028@users.noreply.github.com>
2024-07-21 03:08:38 +0900

    Revert "ボタンを押したらその先へ飛べるようにした"

--- frontend/src/components/CompanyButton.jsx
+++ frontend/src/components/CompanyButton.jsx
@@ -3,7 +3,6 @@ import Button from '@mui/material/Button';
 
 export const CompanyButton = (props) => {
   return (
-    <Button variant="contained"  onClick = {props.onClick}>
-      {props.text}</Button>
+    <Button variant="contained">{props.text}</Button>
   );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,86 +1,86 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
 import { BasicButton } from "../components/BasicButton";
+import { IconButton } from '@mui/material';
 import { CompanyButton } from "../components/CompanyButton";
 import Stack from '@mui/material/Stack';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { SyuCalendar } from "../components/SyuCalender";
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 import { Task } from "../components/Task";
+import Paper from '@mui/material/Paper';
 
-export const Home: React.FC = () => {
-    const navigate = useNavigate();
+export const Home = () => {
     const message1 = "アイコン"
+    const message2 = "追加"
     const message3  = "会社名"
     const message4 = "企業追加"
-
-    const handleCompanyButtonClick = () => {
-        navigate('/company');
-    };
-
-    const handleAddButtonClick = () => {
-        navigate('/companyadd');
-    };
-
     return (
-        <div>
-            <Grid container alignItems="center" justifyContent="center">
-                <h1>ホーム画面</h1>
+    <div>
+        <Grid container alignItems= "center" justifyContent = "center">
+            <h1>ホーム画面</h1>
+        </Grid>
+        <Grid container alignItems= "center" justifyContent = "center">
+            <Grid item xs={2}>
             </Grid>
-            <Grid container alignItems="center" justifyContent="center">
-                <Grid item xs={2}></Grid>
-                <Grid item xs={3}>
-                    <BasicButton text={message1}/>
-                </Grid>
-                <Grid item xs={3}>
-                    <p>名前</p>
-                    <p>所属</p>
-                </Grid>
-                <Grid item xs={1}></Grid>
+            <Grid item xs={3}>
+                <BasicButton text = {message1}/>
             </Grid>
-            <br></br>
-            <Grid container alignItems="center" justifyContent="center">
-                <Stack spacing={30} direction="row" alignItems="center">
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                </Stack>
+            <Grid item xs={3}>
+                <p>名前</p>
+                <p>所属</p>
             </Grid>
-            <br></br>
-            <Grid container alignItems="center" justifyContent="center">
-                <Stack spacing={30} direction="row">
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                </Stack>
+            <Grid item xs={1}>
             </Grid>
-            <br></br>
-            <Grid container alignItems="center" justifyContent="center">
-                <Stack spacing={30} direction="row">
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                    <CompanyButton text={message3} onClick={handleCompanyButtonClick}/>
-                </Stack>
-            </Grid>
-            <br></br>
-            <br></br>
-            <Grid container alignItems="center" justifyContent="center">
-                <BasicButton text={message4} variant="outlined" onClick={handleAddButtonClick}/>
-            </Grid>
-            <br></br>
-            <br></br>
-            <Grid container alignItems="center" justifyContent="center">
+        </Grid>
+        <br></br>
+        <Grid container alignItems= "center" justifyContent = "center">
+            <Stack spacing={30} direction="row" alignItems="center">
+                <CompanyButton text = {message3}/>
+                <CompanyButton text = {message3}/>
+                <CompanyButton text = {message3}/>
+            </Stack>
+        </Grid>
+        <br></br>
+        <Grid container alignItems= "center" justifyContent = "center">
+            <Stack spacing={30} direction="row">
+            <CompanyButton text = {message3}/>
+            <CompanyButton text = {message3}/>
+            <CompanyButton text = {message3}/>
+            </Stack>
+        </Grid>
+        <br></br>
+        <Grid container alignItems= "center" justifyContent = "center">
+            <Stack spacing={30} direction="row">
+            <CompanyButton text = {message3}/>
+            <CompanyButton text = {message3}/>
+            <CompanyButton text = {message3}/>
+            </Stack>
+        </Grid>
+        <br></br>
+        <br></br>
+        <Grid container alignItems= "center" justifyContent = "center">
+            <BasicButton text = {message4} variant="outlined"/>
+        </Grid>
+        <br></br>
+        <br></br>
+            <Grid container alignItems= "center" justifyContent = "center">
+                {/* <Stack spacing={30} direction="row"> */}
                 <Grid item xs={5}>
                     <Paper elevation={3} style={{ padding: '50px', width: '300px' }}>
                         <SyuCalendar />
-                    </Paper>
+                        </Paper>
                 </Grid>
                 <Grid item xs={3}>
-                    <Paper elevation={3} style={{ padding: '20px', width: '400px' }}>
+                <Paper elevation={3} style={{ padding: '20px', width: '400px' }}>
                         <Task />
                     </Paper>
                 </Grid>
+                {/* </Stack> */}
             </Grid>
-        </div>
+    </div>
     );
-};
+  };
+
+ 
+//                     <Task/>
+//                 </Grid>
