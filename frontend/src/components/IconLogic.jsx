dbc1a8ea3a14256bdf7d10428cb1c79877886059
shion200 <shion1390@icloud.com>
2024-07-20 22:41:51 +0900

    Add task

---
+++ frontend/src/components/IconLogic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export const IconLogic = () => {
+  return (
+    <IconButton aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+  );
+};
